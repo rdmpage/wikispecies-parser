4fe8a0e06f4afa908bd6e6335d1e27a35eb345fd
Roderic Page <rdmpage@gmail.com>
2021-12-23 12:41:17 +0000

    Utilities to parse CSL dump

---
+++ parse-csl.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+require_once ('csl_utils.php');
+
+$filename = "all.jsonl";
+
+$count = 0;
+
+$file_handle = fopen($filename, "r");
+while (!feof($file_handle)) 
+{
+	$json = trim(fgets($file_handle));
+		
+    $obj = json_decode($json);
+    
+    //print_r($obj);
+    
+    // $ris = csl_to_ris($obj);
+    
+    //echo $ris;
+    
+	$tsv = csl_to_tsv($obj, $count == 0);
+    
+    echo $tsv . "\n";   
+    
+    $count++;
+    
+    if ($count > 100)    
+    {
+    	exit();
+    }
+    
+	
+}	
+
+?>
